a0d258c6d6b5e6d45d782cd25f97f7c1610d99d7
guyst16 <100173467+guyst16@users.noreply.github.com>
2022-11-22 04:10:48 +0200

    Update utils.py
    Co-authored-by: Doron KG <70585638+doronkg@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import configparser
 import os.path
 
-armadillo_dir_path = os.path.join(os.path.expanduser("~"),".armadillo")
+armadillo_dir_path = os.path.join(os.path.expanduser("~"), ".armadillo")
 policy_file_path = armadillo_dir_path + "/policy.ini"
 dns_policy_path = armadillo_dir_path + "/dns.ini"
 dpi_policy_path = armadillo_dir_path + "/dpi.ini"
